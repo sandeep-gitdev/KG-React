48efff8c9a327d0621b1aafe44aa81569c45eb60
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-08-01 13:22:14 +0530

    bharat clock v-1

---
+++ Projects/2-Bharat-Clock-app-v-01/src/components/ClockHeading.jsx
@@ -0,0 +1,7 @@
+
+  function ClockHeading(){
+        return  <h2 className="fw-bold">Bharat Clock</h2>
+
+  }
+
+  export default ClockHeading;
---
+++ Projects/2-Bharat-Clock-app-v-01/src/components/ClockSlogan.jsx
@@ -0,0 +1,7 @@
+
+
+function ClockSlogan() {
+    return <p className="lead"> This is the clock that show the time in Bharat at all times.</p>
+}
+
+export default ClockSlogan;
