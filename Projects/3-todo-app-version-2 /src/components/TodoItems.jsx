54fb7ff270c34097cb3f0a0d6d953fecab0d1739
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-08-02 15:32:09 +0530

    Todo app version 2

---
+++ Projects/3-todo-app-version-2 /src/components/TodoItems.jsx
@@ -0,0 +1,17 @@
+import TodoItem from "./TodoItem";
+import styles from "./TodoItems.module.css"
+
+function TodoItems({ todoItems }) {
+  
+  return (
+    
+    <div className={styles.itemsContainer}>
+      
+      {todoItems.map((item) => (
+        <TodoItem todoName={item.name} todoDate={item.dueDate}></TodoItem>
+      ))}
+      
+    </div>
+  );
+}
+export default TodoItems;
